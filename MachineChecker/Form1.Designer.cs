964f15c563022e6c82984cf2cc24e892a5929d2e
Asterisk <kuro5858tube@gmail.com>
2018-01-08 21:51:05 +0900

    Add CPU Resourse Logic.

---
+++ MachineChecker/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace MachineChecker
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// chart
+			// 
+			chartArea3.Name = "ChartArea1";
+			this.chart.ChartAreas.Add(chartArea3);
+			legend3.Name = "Legend1";
+			this.chart.Legends.Add(legend3);
+			this.chart.Location = new System.Drawing.Point(0, -1);
+			this.chart.Name = "chart";
+			series3.ChartArea = "ChartArea1";
+			series3.Legend = "Legend1";
+			series3.Name = "Series1";
+			this.chart.Series.Add(series3);
+			this.chart.Size = new System.Drawing.Size(489, 208);
+			this.chart.TabIndex = 0;
+			this.chart.Text = "chart1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(487, 205);
+			this.Controls.Add(this.chart);
+			this.Name = "Form1";
+			this.Text = "CPU Resourse Checker v.1.00";
+			((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+	}
+}
+
---
+++ MachineChecker/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MachineChecker
+{
+	public partial class Form1 : Form
+	{
+		#region Constants
+
+		/// <summary>
+		/// Brousing CPU Graph upper limit.
+		/// </summary>
+		private readonly int MAX_HISTORY = 40;
+
+		#endregion
+
+		#region Field 
+
+		/// <summary>
+		/// CPU Graph Counter.
+		/// </summary>
+		private Queue<int> _countHistory = new Queue<int>();
+
+		/// <summary>
+		/// Using update for CPU Graph.
+		/// </summary>
+		private Timer _timer { get; set; } = new Timer();
+
+		/// <summary>
+		/// Using PerformanceCounter (CPU resourse)
+		/// </summary>
+		private PerformanceCounter _pc { get; set; } 
+			= new PerformanceCounter("Processor", "% Processor Time", "_Total", true);
+
+		#endregion
+
+		#region Constractor
+
+		/// <summary>
+		/// Constractor
+		/// </summary>
+		internal Form1()
+		{
+			InitializeComponent();
+			_initialize(chart);
+
+			_timer.Interval = 1000;
+			_timer.Enabled = true;
+
+			// EventHander for System.Windows.Forms.Timer.
+			_timer.Tick += (s, v) =>
+			{
+				// Getting used CPU Resourse. Added history.
+				_countHistory.Enqueue((int)_pc.NextValue());
+
+				// If it exceeds the maximum number of history, delete the old one.
+				while (_countHistory.Count > MAX_HISTORY)
+					_countHistory.Dequeue();
+
+				// Repaint CPU GraphChart.
+				_BrowsingChart(chart);
+			};
+
+			_timer.Start();
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Initialize from CPU Line Graph.
+		/// </summary>
+		/// <param name="chart"></param>
+		private void _initialize(Chart chart)
+		{
+			// Set the background color of the entire chart
+			chart.BackColor = Color.DarkBlue;
+			chart.ChartAreas[0].BackColor = Color.Transparent;
+
+			// Cut the margin around the chart display area
+			chart.ChartAreas[0].InnerPlotPosition.Auto = false;
+			chart.ChartAreas[0].InnerPlotPosition.Width = 100;
+			chart.ChartAreas[0].InnerPlotPosition.Height = 90; 
+			chart.ChartAreas[0].InnerPlotPosition.X = 8;
+			chart.ChartAreas[0].InnerPlotPosition.Y = 0;
+
+			// Define X, Y axis information set function
+			Action<Axis> setAxis = (axisInfo) => {
+
+				// Limit font size upper limit of axis memory label
+				axisInfo.LabelAutoFitMaxFontSize = 8;
+
+				// Set character color of axis memory label
+				axisInfo.LabelStyle.ForeColor = Color.White;
+
+				// Set axis color
+				axisInfo.MajorGrid.Enabled = true;
+				axisInfo.MajorGrid.LineColor = ColorTranslator.FromHtml("#008242");
+				axisInfo.MinorGrid.Enabled = false;
+				axisInfo.MinorGrid.LineColor = ColorTranslator.FromHtml("#008242");
+			};
+
+			// Define display method of X and Y axes
+			setAxis(chart.ChartAreas[0].AxisY);
+			setAxis(chart.ChartAreas[0].AxisX);
+
+			chart.ChartAreas[0].AxisX.MinorGrid.Enabled = true;
+			chart.ChartAreas[0].AxisY.Maximum = 100;
+
+			chart.AntiAliasing = AntiAliasingStyles.None;
+
+			// Line graph
+			chart.Series[0].ChartType = SeriesChartType.FastLine;
+
+			// Designate line color
+			chart.Series[0].Color = ColorTranslator.FromHtml("#00FF00");
+
+			// Hide the legend, do not display numbers on each value
+			chart.Series[0].IsVisibleInLegend = false;
+			chart.Series[0].IsValueShownAsLabel = false;
+
+			// Clear all the history of values displayed on the chart to 0
+			while (_countHistory.Count <= MAX_HISTORY)
+				_countHistory.Enqueue(0);
+				
+		}
+
+		/// <summary>
+		/// Browsing from CPU Line Graph.
+		/// </summary>
+		/// <param name="chart"></param>
+		private void _BrowsingChart(Chart chart)
+		{
+			chart.Series[0].Points.Clear();
+			foreach (int value in _countHistory)
+				chart.Series[0].Points.Add(new DataPoint(0, value));
+		}
+
+		#endregion
+
+	}
+}
